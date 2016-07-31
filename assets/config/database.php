6397824dd03b286557d781fb92e329f966f83439
suspensk <uspenskayasofyasergeevna@gmail.com>
2016-07-31 13:46:10 +0300

    db name

--- assets/config/database.php
+++ assets/config/database.php
@@ -4,7 +4,7 @@ $root = realpath(__DIR__.'/../../');
 return array(
     'default' => array(
         'driver' => 'pdo',
-        'connection' => 'mysql:host=suspensk.mysql;dbname=suspensk',
+        'connection' => 'mysql:host=suspensk.mysql;dbname=suspensk_db',
         'user'     => 'suspensk_mysql',
         'password' => '8UDvYJ_O'
     )
