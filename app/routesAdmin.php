8345672fd082f8d0ec992ea018a1cdb72ab8846d
suspensk <uspenskayasofyasergeevna@gmail.com>
2016-12-14 16:34:53 +0200

    admin home view

--- app/routesAdmin.php
+++ app/routesAdmin.php
@@ -5,4 +5,90 @@ use App\Middleware\Admin\GuestMiddleware;
 //    return $this->view->render($response, 'home.twig');
 //});
 
-$app->get('/admin/','HomeController:index')->setName('home');
+
+
+$app->group('', function(){
+
+    $this->get('/admin/','HomeController:index')->setName('home');
+    $this->get('/admin/auth/signout','AuthController:getSignOut')->setName('auth.signout');
+    $this->get('/admin/auth/password/change','PasswordController:getChangePassword')->setName('auth.password.change');
+    $this->post('/admin/auth/password/change','PasswordController:postChangePassword');
+
+    $this->get('/admin/{controller}/{action}', function ($request, $response, $args) {
+        $query = (object)$request->params();
+
+        var_dump($args);
+        die();
+
+//	if (!class_exists($controller)) {
+//        throw new InvalidArgumentException("The action controller '$controller' has not been defined.");
+//    }
+//	$controller = new $controller($query, $user);
+//	$controller = new $controller($query, $user, $detect, $logger);
+//	if (!method_exists($controller, $action)) {
+//        throw new InvalidArgumentException("The method '$action' of '$controller' don't exists.");
+//        $action = 'index';
+//        // throw new InvalidArgumentException("The method '$action' of '$controller' don't exists.");
+//    }
+//	$controller->{$action}();
+
+    });
+
+    $this->get('/admin/{controller}', function ($request, $response, $args) {
+        var_dump($args);
+        die();
+
+    });
+
+
+
+
+
+
+})->add(new AuthMiddleware($container));
+
+
+
+//$app->group('', function(){
+//    $this->get('/admin/auth/signup','AuthController:getSignUp')->setName('auth.signup');
+//    $this->post('/admin/auth/signup','AuthController:postSignUp');
+//
+//    $this->get('/admin/auth/signin','AuthController:getSignIn')->setName('auth.signin');
+//    $this->post('/admin/auth/signin','AuthController:postSignIn');
+//})->add(new GuestMiddleware($container));
+
+
+/**/
+
+
+//$app->get('/{controller}/{action}', function ($request, $response, $args) use ($app)  {
+//  //  echo $args['controller'];
+// //   var_dump($request->getParams());
+//    var_dump($args);
+//
+//});
+
+//$app->get('/:controller(/:action)/', function ($controller, $action = null) use ($app) {
+//
+//    var_dump($controller);
+//    die();
+//    $query = (object)$app->request->params();
+//
+//    $controller = ucfirst($controller) . 'Controller';
+//
+//    if (!class_exists($controller)) {
+//        throw new InvalidArgumentException("The action controller '$controller' has not been defined.");
+//    }
+//    $controller = new $controller($query, $user, $detect, $logger);
+//    if (!method_exists($controller, $action)) {
+//        $action = 'index';
+//        // throw new InvalidArgumentException("The method '$action' of '$controller' don't exists.");
+//    }
+//    $controller->{$action}();
+
+
+//});
+
+//$app->notFound(function () use ($app) {
+//    var_dump($app->request);
+//});
